e9ee789d5b98d6fc639c061a131f0ab389e14a40
xdbaqiao <yexj061761@gmail.com>
2013-04-01 20:38:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Interesting shell scripts.
+=======
+**Change the zsh prompt**    
+        
+You can select face expression from  "\#>_<", "^_^", "T_T", "$_$", "(￣﹁￣)", "^(oo)^" randomly.                      
+It's so funny!          
+    
+**Result:**          
+       `yexinjing@vipsl ~ ^_^`         
+       `yexinjing@vipsl ~ T_T`        
+       `yexinjing@vipsl ~ >_<`         
+       `yexinjing@vipsl ~ ^(oo)^`           
+       `yexinjing@vipsl ~ (￣﹁￣)`       
+       `yexinjing@Paradise ~ $_$ `          
---
+++ face_zsh.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#>_<, ^_^, T_T, $_$, (￣﹁￣), ^(oo)^
+
+file=/home/yexinjing/.zshrc
+output_file=/tmp/zshrc.tmp
+
+face=('>_<' '\^_\^' 'T_T' '\$_\$' '(￣﹁￣)' '\^(oo)\^')
+
+
+if [ ! $1 ];then
+    let num=$(($RANDOM % 6))
+    chg=${face[$num]}
+else
+    chg=$1
+fi
+
+echo $chg
+
+for i in `seq 6`;
+do
+    sig=${face[$i-1]}
+    cat "$file" |grep "$sig" >/dev/null
+    if [ $? -eq 0 -a "$sig" ];then
+        tmp='s/'"$sig"'/'$chg'/'
+        echo $tmp
+        sed -e $tmp $file > $output_file
+        if [ $? -eq 1 ];then
+            break
+        fi
+        rm $file
+        cp $output_file $file
+        break
+    fi
+done
